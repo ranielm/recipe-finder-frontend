fbb4fb5cce7ffc268a443638cbf17a07d34a7c8c
Raniel Mendonça <raniel.mendonca@luizalabs.com>
2023-11-03 14:13:44 -0300

    fix vite env

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import dotenv from 'vite-plugin-env-compatible';
 
 export default defineConfig({
-  plugins: [react(), dotenv()],
+  plugins: [react()],
 });
