3fff60c74158c7d61742b4a5018f267d8c86acf2
Raniel Mendonça <raniel.mendonca@luizalabs.com>
2023-11-05 15:05:05 -0300

    adding styled components

--- src/containers/AppLayout/AppLayout.tsx
+++ src/containers/AppLayout/AppLayout.tsx
@@ -3,11 +3,18 @@ import {
   createTheme,
   ThemeProvider,
   CssBaseline,
-  GlobalStyles,
   PaletteMode,
   Button,
 } from '@mui/material';
 import { IRequireAuthProps } from '../../components/RequireAuth/RequireAuth.types';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  input:-webkit-autofill {
+    -webkit-box-shadow: 0 0 0 1000px transparent inset !important;
+    transition: background-color 5000s ease-in-out 0s !important;
+  }
+`;
 
 const AppLayout: FC<IRequireAuthProps> = ({ children }) => {
   const [mode, setMode] = useState<PaletteMode>('dark');
@@ -28,14 +35,8 @@ const AppLayout: FC<IRequireAuthProps> = ({ children }) => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <GlobalStyles
-        styles={{
-          'input:-webkit-autofill': {
-            WebkitBoxShadow: '0 0 0 1000px transparent inset !important',
-            transition: 'background-color 5000s ease-in-out 0s !important',
-          },
-        }}
-      />
+      <GlobalStyles />
+
       <Button onClick={toggleTheme}>Toggle Theme</Button>
       {children}
     </ThemeProvider>
